63662cf827f91c5199b84ae7d90b10595320cd93
Rao Abrar Nawaz <fa23-rcs-014@cuilahore.edu.pk>
2024-07-09 11:07:29 +0500

    added migrations of the created models

--- Eshop/models.py
+++ Eshop/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=250)
     description = models.TextField()
-    price = models.DecimalField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     
     def __str__(self):
         return self.name
