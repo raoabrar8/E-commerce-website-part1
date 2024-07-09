da102411e75982a7d0c20ecb223002ea1e7f7d9e
Rao Abrar Nawaz <fa23-rcs-014@cuilahore.edu.pk>
2024-07-09 12:54:13 +0500

    added user view

--- Eshop/views.py
+++ Eshop/views.py
@@ -7,7 +7,7 @@ from .forms import CategoryForm, ProductForm
 
 def is_admin(user):
     return user.is_staff
-
+# Admin Interface Views
 
 @user_passes_test(is_admin)
 def admin_dashboard(request):
@@ -48,3 +48,48 @@ def delete_category(request, pk):
     category.delete()
     return redirect('admin_dashboard')
 
+@user_passes_test(is_admin)
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_dashboard')
+        
+    else:
+        form = ProductForm()
+        
+    return render(request, 'Admin/add_product.html', {'form':form})
+
+@user_passes_test(is_admin)
+def update_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance = product)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_dashboard')
+        
+    else:
+        form = ProductForm()
+    return render(request, 'Admin/update_product.html', {'form':form})
+
+
+@user_passes_test(is_admin)
+def delete_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    product.delete()
+    return redirect('admin_dahsboard')
+
+
+# User Interface Views
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'User/category_list.html', {'categories':categories})
+
+def product_list(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    products = category.products.all()
+    return render(request, 'User/product_list.html', {'category':category, 'products':products})
