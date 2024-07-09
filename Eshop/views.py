6c83da0a791ae77ed11e1201f6998a351c36b184
Rao Abrar Nawaz <fa23-rcs-014@cuilahore.edu.pk>
2024-07-09 12:18:27 +0500

    added the updated and delete view of the category

--- Eshop/views.py
+++ Eshop/views.py
@@ -9,14 +9,14 @@ def is_admin(user):
     return user.is_staff
 
 
-@user_passes_test
+@user_passes_test(is_admin)
 def admin_dashboard(request):
     products = Product.objects.all()
     categories = Category.objects.all()
     return render(request, 'Admin/admin_dashboard.html', {'categories':categories, 'products':products})
 
 
-@user_passes_test
+@user_passes_test(is_admin)
 def add_category(request):
     if request.mehtod == 'POST':
         form = CategoryForm(request.POST)
@@ -27,4 +27,24 @@ def add_category(request):
     else:
         form = CategoryForm()
     return render(request, 'Admin/add_category.html', {'form':form})
+
+@user_passes_test(is_admin)
+def update_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance = category)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_dashboard')
+        
+    else:
+        form = CategoryForm()
+    return render(request, 'Admin/update_category.html', {'form':form})
+
+
+@user_passes_test(is_admin)
+def delete_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    category.delete()
+    return redirect('admin_dashboard')
 
