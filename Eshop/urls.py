2c4efad2552fd7694b992df4ebc3e4ca5059aece
Rao Abrar Nawaz <fa23-rcs-014@cuilahore.edu.pk>
2024-07-09 13:13:25 +0500

    added urls for the browsing

--- Eshop/urls.py
+++ Eshop/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('admin/', views.admin_dashboard, name='admin_dashboard'),
+    path('admin/add_category/', views.add_category, name='add_category'),
+    path('admin/update_category/<int:pk>/', views.update_category, name='update_category'),
+    path('admin/delete_category/<int:pk>/', views.delete_category, name='delete_category'),
+    path('admin/add_product/', views.add_product, name='add_product'),
+    path('admin/update_prdoduct/<int:pk>', views.update_product, name='update_prodcut'),
+    path('admin/delete_prodcut/<int:pk>', views.delete_product, name='delete_prodcut'),
+    path('', views.category_list, name='category_list'),
+    path('category/<int:category_id>/', views.product_list, name='product_list')
 ]
